50445292276019765384795b272fb716607d65cf
Ivan Gorstkin <ivan.orlovsky.v@gmail.com>
2018-11-10 21:31:40 +0300

    Absolute position for form errors

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Route, Switch, Router} from 'react-router-dom';
 import Main from "../components/Main";
 import history from './history';
-import Login from "../pages/Signin";
+import Signin from "../pages/Signin";
 import Error404 from "../pages/Error404";
 
 
@@ -10,8 +10,8 @@ const AppRouter = props => {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path='/' component={Login}/>
-        <Route exact path='/login' component={Login}/>
+        <Route exact path='/' component={Signin}/>
+        <Route exact path='/signin' component={Signin}/>
         <Main>
           <Switch>
             <Route component={Error404}/>
